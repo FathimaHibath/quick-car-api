32f809c3cf185a8ae64925e921e94767ea90ffa7
Fathima <fathimahibathk@gmail.com>
2025-07-30 05:17:13 +0530

    Use Firebase key from environment variable

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from flask_cors import CORS
+import json
 
 # Initialize Flask App
 app = Flask(__name__)
