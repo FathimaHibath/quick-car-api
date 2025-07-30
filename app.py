1b6b491ca04de7ccd794674ee34867d27421422b
Fathima <fathimahibathk@gmail.com>
2025-07-30 05:34:54 +0530

    Fix: added missing os import

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from flask_cors import CORS
 import json
+import os
+
 
 # Initialize Flask App
 app = Flask(__name__)
